ef01c3246afcbbf953e97449ca56c92e38578725
AUDO <nilswittig96@gmail.com>
2018-11-13 16:01:24 +0100

    worked on contour_detection did not get far

--- contour_detection/src/contour_detection_node.cpp
+++ contour_detection/src/contour_detection_node.cpp
@@ -4,15 +4,18 @@
 #include <std_msgs/String.h>
 #include <opencv2/core/ocl.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
 //#include <sensor_msgs/CompressedImage.h>
 #include <sensor_msgs/Image.h>
+#include <sensor_msgs/image_encodings.h>
+#include <image_transport/image_transport.h>
 
 
 
 
 
-
-
+static const std::string OPENCV_WINDOW = "Image window";
 
 //  /kinect2/qhd/image_color/compressed
 
@@ -22,9 +25,23 @@
 
 //const image_msg::ConstPtr& img
 
-void imageCallback(const sensor_msgs::Image::ConstPtr& img){
+void imageCallback(const sensor_msgs::ImageConstPtr& msg){
     
     ROS_INFO("I heard:vvv");
+
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+    // Update GUI Window
+    //cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+
     //ROS_INFO("I heard: %v", img->data);
 }
 
@@ -36,25 +53,19 @@ int main(int argc, char** argv)
   // get ros node handle
   ros::NodeHandle nh;
 
-  // sensor message container
-  std_msgs::Int16 motor, steering;
-
-  /*
-  // generate control message publisher
-    ros::Publisher motorCtrl =
-      nh.advertise<std_msgs::Int16>("/uc_bridge/set_motor_level_msg", 1);
-  ros::Publisher steeringCtrl =
-      nh.advertise<std_msgs::Int16>("/uc_bridge/set_steering_level_msg", 1);
-  ros::Publisher chatter = nh.advertise<std_msgs::String>("chatter",10);
-  */
-  //ros::Subscriber sub = nh.subscribe("/kinect2/qhd/image_color/compressed", 10, imageCallback);
-
-  //ros::Subscriber sub = nh.subscribe<image_msg>("kinect2/qhd/image_color", 10, imageCallback);
-  //ros::Subscriber sub = nh.subscribe<sensor_msgs::Image>("kinect2/qhd/image_color", 10, imageCallback);
-  ros::Subscriber sub = nh.subscribe("kinect2/qhd/image_color", 10, imageCallback);
+  //cv::namedWindow(OPENCV_WINDOW);
+  
+  image_transport::ImageTransport it(nh);
+  image_transport::Subscriber sub = it.subscribe("kinect2/qhd/image_color", 1, imageCallback);
+  //image_transport::Publisher pub = it.advertise("out_image_base_topic", 1);
+  
+  
+  
 
   ROS_INFO("Contour Detection Start");
 
+  
+  
   // Loop starts here:
   // loop rate value is set in Hz
   ros::Rate loop_rate(0.5);
