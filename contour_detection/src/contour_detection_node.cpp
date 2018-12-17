53109a83d1504fbd23b63ad89683ad9b891f4222
AUDO <nilswittig96@gmail.com>
2018-12-17 19:32:34 +0100

    AUDO is now able to follow lines at 10Hz

--- contour_detection/src/contour_detection_node.cpp
+++ contour_detection/src/contour_detection_node.cpp
@@ -20,8 +20,7 @@ static const std::string OPENCV_WINDOW = "Image window";
 static const std::string OPENCV_RAW = "Image RAAAAAWW window";
 static const std::string OPENCV_STRANGE = "Image filtered window";
 
-cv::Point upper;
-cv::Point lower;
+int x_now;
 
 // Are the two points set?
 int is_set = 0;
@@ -53,17 +52,17 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg){
     cv::Point2f inputQuad[4]; 
     // The 4 points that select quadilateral on the input , from top-left in clockwise order
     // These four pts are the sides of the rect box used as input 
-    inputQuad[0] = cv::Point2f( 1,400 ); //topleft
-    inputQuad[1] = cv::Point2f( 1279,400 ); //topright
-    inputQuad[2] = cv::Point2f( 1279,719 ); //bottomright
-    inputQuad[3] = cv::Point2f( 1,719  ); //bottomleft
+    inputQuad[0] = cv::Point2f( 1,150 ); //topleft
+    inputQuad[1] = cv::Point2f( 639,150 ); //topright
+    inputQuad[2] = cv::Point2f( 639,479 ); //bottomright
+    inputQuad[3] = cv::Point2f( 1,479  ); //bottomleft
     // Output Quadilateral or World plane coordinates
     cv::Point2f outputQuad[4];
     // The 4 points where the mapping is to be done , from top-left in clockwise order
     outputQuad[0] = cv::Point2f( 1,1 ); //topleft mapped
-    outputQuad[1] = cv::Point2f( 1279,1 ); //topright mapped
-    outputQuad[2] = cv::Point2f( 800,719 ); //bottomright mapped
-    outputQuad[3] = cv::Point2f( 300,719  ); //bottomleft mapped
+    outputQuad[1] = cv::Point2f( 639,1 ); //topright mapped
+    outputQuad[2] = cv::Point2f( 380,439 ); //bottomright mapped
+    outputQuad[3] = cv::Point2f( 260,439  ); //bottomleft mapped
 
     // Lambda Matrix
     cv::Mat lambda;
@@ -77,12 +76,14 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg){
     // #####################################################################################################
     
     // Set Region of Interest
-    int offset_x = 300;
-    int offset_y = 0;
+    int offset_x = 260;
+    int offset_y = 100;
     cv::Rect roi;
     roi.x = offset_x;
     roi.y = offset_y;
-    roi.width = bird.size().width - (offset_x*2);
+    //roi.width = bird.size().width - (offset_x*2);
+    roi.width = 120;
+    //roi.height = bird.size().height - 150;
     roi.height = bird.size().height - 150;
     //Crop the original image to the defined ROI
     cv::Mat crop = bird(roi);
@@ -93,8 +94,8 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg){
     // #####################################################################################################
     //cv::Mat brighton = cv::Mat::zeros(crop.size(), CV_8UC3);
     double alpha = 1.0; /*< Simple contrast control [1.0-3.0]*/
-    int beta = 10;       /*< Simple brightness control [0-100]*/
-    
+    int beta = 0;       /*< Simple brightness control [0-100]*/
+    /*
     for( int y = 0; y < crop.rows; y++ ) {
         for( int x = 0; x < crop.cols; x++ ) {
             for( int c = 0; c < crop.channels(); c++ ) {
@@ -103,7 +104,7 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg){
             }
         }
     }
-    
+    */
     cv::imshow(OPENCV_STRANGE, crop);
     cv::waitKey(3);
     // #####################################################################################################
@@ -116,124 +117,60 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg){
     // only keep green
     cv::Mat hsv_filtered   = cv::Mat::zeros( crop.size(), CV_8UC3 );
     //cv::inRange(hsv1, cv::Scalar(50, 20, 100), cv::Scalar(70, 255, 255), hsv_filtered);
-    cv::inRange(crop, cv::Scalar(40, 30, 100), cv::Scalar(70, 255, 255), hsv_filtered);
+    cv::inRange(crop, cv::Scalar(62, 20, 100), cv::Scalar(78, 255, 255), hsv_filtered);
     
     // the image is now a grayscale
 
+    // blur the image
     cv::Mat blur = cv::Mat::zeros(crop.size(), CV_8UC3);
     cv::GaussianBlur( hsv_filtered, blur, cv::Size( 15, 15 ), 0, 0 );
     
-    cv::imshow(OPENCV_RAW, blur);
-    cv::waitKey(3);
+    //cv::imshow(OPENCV_RAW, blur);
+    //cv::waitKey(3);
 
-    // #####################################################################################################
-    // #####  Convert the image from grayscale to binary (only black or white)                ##############
-    // #####################################################################################################
-    
-    cv::Mat bin;
-    //cv::threshold( hsv_filtered, bin, threshold_value, max_BINARY_value,threshold_type );
-    cv::threshold( blur, bin, 245, 255,CV_THRESH_BINARY );
-
-    // #####################################################################################################
-    // #####  Detect straight lines with the probalistic Hough transformation                 ##############
-    // #####################################################################################################    
-
-    std::vector<cv::Vec4i> lines;
-    //threshold: The minimum number of intersections to “detect” a line
-    //minLinLength: The minimum number of points that can form a line. Lines with less than this number of points are disregarded.
-    //maxLineGap: The maximum gap between two points to be considered in the same line.
-    //cv::HoughLinesP(bin, lines, 1, CV_PI/180, threshold, minLinLength, maxLineGap );
-    cv::HoughLinesP(bin, lines, 1, CV_PI/180, 100, 100, 50 );
-
-    if(lines.size() == 0){
-      return; // there is nothing more to do
-    }
-
-    // calculate some more information about the found lines
-    ROS_INFO("lines:\t%i",(int)(lines.size()));
-    int lengths[lines.size()];
-    double alphas[lines.size()];
-    for( int i = 0; i < lines.size(); i++ ) {
-      cv::Vec4i vec = lines[i];
-      lengths[i] = sqrt( pow( abs( vec[2] - vec[0] ), 2 ) + pow( abs( vec[3] - vec[1] ), 2 ) );
-      alphas[i] = asin( (double) (abs( vec[3] - vec[1] )) / (double)(lengths[i]) ) * 180 / M_PI;
-      //ROS_INFO("line[%i]: x1 = %i, y1 = %i, x2 = %i; y2 = %i, length = %i, alpha = %f",i,vec[0],vec[1],vec[2],vec[3],lengths[i],alphas[i]);
-    }
-    // To Do: write code for the left side
-    // CASE: driving on the right side
-    // search for the line with smallest x and biggest y start point
-    int x_min = 300;
-    int y_max = 0;
-    for( int i = 0; i < lines.size(); i++ ) {
-      cv::Vec4i vec = lines[i];
-      if( x_min > vec[2] && vec[3] > 300){
-        x_min = vec[2];
-        if( y_max < vec[3]){
-          y_max = vec[3];
+    // find the lowest x-point of the line
+    int x_ding = 80; // should be the lowest x-point 
+    int y_ding = 30;
+    // down most 45 lines 
+    // find middle of the white stripe
+    for( int y = 280; y < blur.rows - 15; y++ ) {
+        for( int x = 0; x < blur.cols - 5; x++ ) {
+            
+            if(  100 < ( blur.at<uchar>(x,y)
+              + blur.at<uchar>(y,x + 1 )
+              + blur.at<uchar>(y,x + 2)
+              + blur.at<uchar>(y,x + 3)
+              + blur.at<uchar>(y,x + 4)
+            )
+            ){
+                //yaay
+                x_ding = x + 1;
+                y_ding = y;
+                break;
+            }
         }
-      }
     }
-    // find the relevant line
-    int start = 0;
-    for( int i = 0; i < lines.size(); i++ ) {
-      cv::Vec4i vec = lines[i];
-      if( x_min + 10 >= vec[2] 
-          && vec[2] >= x_min - 10 
-          && y_max + 10 >= vec[3] 
-          && vec[3] >= y_max - 10 ){
-        start = i;
-        break;
-      }
-    }
-    // small hack if the relevant line was not found correctly - works surprisingly well
-    if(start == 0){
-      start = 1;
-    }
-    ROS_INFO("start = %i",start);
-    
-    // AUDO is in the middle of the right track if the start point of the line is at x = 260px
-    // height = 390
-    // width = 360
-    // blindspot length = 35cm, meassured from alu case
-    // 10cm_real = 4,3cm_screen
-    // pixel/cm_real = 15,97
-    // Extend line
-    cv::Vec4i vec = lines[start];
-    double m = ( (double)(vec[2]) - (double)(vec[0]) ) / ( (double)(vec[3]) - (double)(vec[1]) );
-    double b = vec[2] - m * vec[3];
-    upper = cv::Point((int)( vec[0] - m * vec[1] ),0);
-    lower = cv::Point((int) (m * bin.size().width + b), (int) bin.size().height);
-    
+    x_now = x_ding;
+    ROS_INFO("Lowest X of line:\t%i",x_ding);
+
     // mark the points as set 
     is_set = 1;
     // mark the points as updated
     new_points = 1;
 
-
-    ROS_INFO("m = %f, Upper = (%i,%i), Lower = (%i,%i)", m, upper.x, upper.y, lower.x, lower.y);
-    ROS_INFO("line[%i]: x1 = %i, y1 = %i, x2 = %i; y2 = %i, length = %i, alpha = %f",start,vec[0],vec[1],vec[2],vec[3],lengths[start],alphas[start]);
-
     // visualize for debug
     cv::Mat bgr  = cv::Mat::zeros( hsv_filtered.size() * 3, CV_8UC3 );
-    cv::cvtColor(hsv_filtered, bgr,  cv::COLOR_GRAY2BGR, 3);
+    cv::cvtColor(blur, bgr,  cv::COLOR_GRAY2BGR, 3);
     cv::Scalar red = cv::Scalar( 0, 0, 255 );
     cv::Scalar blue = cv::Scalar( 255, 0, 0 );
     // display the relevant line
-    line(bgr, cv::Point(vec[2],vec[3]), cv::Point(vec[0],vec[1]), red, 3, 8, 0);
-    line(bgr, lower, upper, blue, 2, 8, 0);
+    //line(bgr, cv::Point(vec[2],vec[3]), cv::Point(vec[0],vec[1]), red, 3, 8, 0);
+    //line(bgr, lower, upper, blue, 2, 8, 0);
+    cv::rectangle( bgr, cv::Point(x_ding,y_ding), cv::Point(x_ding,y_ding) + cv::Point( 2,2 ), red, 2 );
     // show image
     cv::imshow(OPENCV_WINDOW, bgr);
     cv::waitKey(3);
     
-
-    // Color filter attempts for other Webcam
-    //cv::inRange(hsv, cv::Scalar(100, 20, 100), cv::Scalar(115, 255, 255), hsv_filtered);
-    // H 107
-    // S 255
-    // V 224
-    //cv::GaussianBlur( hsv_filtered, lowPassed, Size( 5, 5 ), 2, 2 );
-    // cv::Mat lowPassed;
-    //cv::bilateralFilter(hsv_filtered, lowPassed, 5, 120, 120);
   
 }
 
@@ -246,22 +183,20 @@ int main(int argc, char** argv)
   ros::NodeHandle nh;
 
   // create a window the show things
+  
   cv::namedWindow(OPENCV_WINDOW);
   cv::waitKey(3);
   cv::namedWindow(OPENCV_STRANGE);
   cv::waitKey(3);
-  cv::namedWindow(OPENCV_RAW);
-  cv::waitKey(3);
+  //cv::namedWindow(OPENCV_RAW);
+  //cv::waitKey(3);
   
   image_transport::ImageTransport it(nh);
-  //image_transport::Subscriber sub = it.subscribe("cv_camera/image_raw", 1, imageCallback);
-  image_transport::Subscriber sub = it.subscribe("kinect2/qhd/image_color", 1, imageCallback);
+  image_transport::Subscriber sub = it.subscribe("cv_camera/image_raw", 1, imageCallback);
+  //image_transport::Subscriber sub = it.subscribe("kinect2/qhd/image_color", 1, imageCallback);
   
   // publish the line as four points
   ros::Publisher x1 = nh.advertise<std_msgs::Int32>("/line_recoqnition/x1", 1);
-  ros::Publisher y1 = nh.advertise<std_msgs::Int32>("/line_recoqnition/y1", 1);
-  ros::Publisher x2 = nh.advertise<std_msgs::Int32>("/line_recoqnition/x2", 1);
-  ros::Publisher y2 = nh.advertise<std_msgs::Int32>("/line_recoqnition/y2", 1);
   
 
 
@@ -270,7 +205,7 @@ int main(int argc, char** argv)
   
   // Loop starts here:
   // loop rate value is set in Hz
-  ros::Rate loop_rate(0.5);
+  ros::Rate loop_rate(10);
   while (ros::ok())
   {
 
@@ -279,22 +214,10 @@ int main(int argc, char** argv)
       if( new_points ){
         // only publish points if they where updated
         new_points = 0;
-
         std_msgs::Int32 x_1;
-        std_msgs::Int32 y_1;
-        std_msgs::Int32 x_2;
-        std_msgs::Int32 y_2;
-
-        x_1.data = lower.x;
-        y_1.data = lower.y;
-        x_2.data = upper.x;
-        y_2.data = upper.y;
-
+        x_1.data = x_now;
         // publish the points
         x1.publish( x_1 );
-        y1.publish( y_1 );
-        x2.publish( x_2 );
-        y2.publish( y_2 );
       }
     }
     // clear input/output buffers
--- wallfollow/src/wallfollow_node.cpp
+++ wallfollow/src/wallfollow_node.cpp
@@ -11,41 +11,7 @@
 double roll, pitch, yaw, last_t, t;
 std_msgs::Int16 motor, steering;
 bool stop = false;
-// current position
-geometry_msgs::Point pos;
-// position from when received current image
-geometry_msgs::Point last_pos;
-// received new image
-int new_image = 1;
-
 double x_1;
-double y_1;
-double x_2;
-double y_2;
-
-// return the distance between AUDO and line in cm
-double get_distance(){
-    /*
-    // x and y coords are in 0.5m 
-    double current_y = pos.x * 50;
-    double current_x = pos.y * 50;
-    double last_x = last_pos.x * 50;
-    double last_y = last_pos.y * 50;
-    // calculate y offset from last_pos
-    double y_offset = current_y - last_y;
-    double x_offset = current_x - last_x;
-    
-    // calculate distance with offset with help of m
-    double m = ( x_2 - x_1 ) / ( y_2 - y_1 );
-    // where is the line now?
-    
-    double x_now = (x_1 / 16) - y_offset * m; //cm 
-    // x_now is the distance in pixels from the line
-    // x_now should be regualated to be 260 at all times
-    */
-    return ( x_1 - 180.0 ) / 160.0;
-}
-
 
 void odomCallback(nav_msgs::Odometry::ConstPtr odomMsg, nav_msgs::Odometry* odom)
 {
@@ -56,44 +22,14 @@ void odomCallback(nav_msgs::Odometry::ConstPtr odomMsg, nav_msgs::Odometry* odom
     tf::quaternionMsgToTF(odom->pose.pose.orientation, q);
     tf::Matrix3x3 mat(q);
     mat.getEulerYPR(yaw, pitch, roll);
-
-    // set image received position
-    if( new_image ){
-        last_pos = odom->pose.pose.position;
-        new_image = 0;
-    }
-    // get Position
-    pos = odom->pose.pose.position;
     
 }
 
 void x1_Callback(std_msgs::Int32::ConstPtr msg, double* data)
 {
-  // inform odomCallback that a new image arrived
-  new_image = 1;
   *data = (double) msg->data;
-  //ROS_INFO("x_1 = %f",x_1);
-            // TODO:
-    // get values from odom.pose.pose.position (and odom.twist.twist.linear.x an odom.twist.twist.linear.y)
-    // if new image from contour_detection is received
-    //  then:   save position of audo, save line to follow
-    //          reset position = 0
-    // if  wallfollow wants to calc new value and no new image is available
-    //  then:   use odomerty position for calculation until next image is received
+  
 }
-void y1_Callback(std_msgs::Int32::ConstPtr msg, double* data)
-{
-  *data = (double) msg->data;
-}
-void x2_Callback(std_msgs::Int32::ConstPtr msg, double* data)
-{
-  *data = (double) msg->data;
-}
-void y2_Callback(std_msgs::Int32::ConstPtr msg, double* data)
-{
-  *data =(double)  msg->data;
-}
-
 
 // gets called whenever a new message is availible in the input puffer
 void uslCallback(sensor_msgs::Range::ConstPtr uslMsg, sensor_msgs::Range* usl)
@@ -142,15 +78,7 @@ int main(int argc, char** argv)
   // subscribe to the line_recoqnition
   ros::Subscriber x1_sub = nh.subscribe<std_msgs::Int32>(
       "/line_recoqnition/x1", 1, boost::bind(x1_Callback, _1, &x_1));
-  ros::Subscriber y1_sub = nh.subscribe<std_msgs::Int32>(
-      "/line_recoqnition/y1", 1, boost::bind(y1_Callback, _1, &y_1));
-  ros::Subscriber x2_sub = nh.subscribe<std_msgs::Int32>(
-      "/line_recoqnition/x2", 1, boost::bind(x2_Callback, _1, &x_2));
-  ros::Subscriber y2_sub = nh.subscribe<std_msgs::Int32>(
-      "/line_recoqnition/y2", 1, boost::bind(y2_Callback, _1, &y_2));
   
-
-
   // generate control message publisher
   ros::Publisher motorCtrl =
       nh.advertise<std_msgs::Int16>("/uc_bridge/set_motor_level_msg", 1);
@@ -162,7 +90,7 @@ int main(int argc, char** argv)
 
   ROS_INFO("A simple Wallfollow");
 
-  double sollwert = 0.1;
+  double sollwert = 0.8;
   int pd = 200;
   int pk = 800;
   double last_err = 0;
@@ -179,23 +107,16 @@ int main(int argc, char** argv)
     t = ((double)clock()/CLOCKS_PER_SEC);
     int s_out = 0;
 
-
-   // if(new_image){
-   //     last_pos = pos;
-   //     pos = 0;
-        abstand = get_distance();
-        ROS_INFO("Abstand = %f",abstand);
-   //     new_image = 0;
-   // }
+    abstand = (double)x_1 / 100.0 ;
+    ROS_INFO("Abstand = %f",abstand);
     
-
     // Regelabweichung
-   // err = sollwert - usr.range*cos(yaw);
+    // err = sollwert - usr.range*cos(yaw);
     err = sollwert - abstand /* * cos(yaw)*/;
 
     d_err = (err-last_err);
 
-    s_out = -(pk * err + pd * d_err / (t - last_t) );
+    s_out = -(pk * err + pd * d_err / (t - last_t) ) *5;
     if(s_out > 600) s_out = 600;
     else if(s_out < -600) s_out = -600;
 
@@ -226,9 +147,6 @@ int main(int argc, char** argv)
     
     last_err = err;
     last_t = t;
-
-
-
 
     // clear input/output buffers
     ros::spinOnce();
